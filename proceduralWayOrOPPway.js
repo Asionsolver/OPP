afae600b1edf9922f002c3acefc8e3ccb147ffd8
Asionsolver <asionsolver274@gmail.com>
2023-06-12 20:02:49 +0600

    changeFie name

---
+++ proceduralWayOrOPPway.js
@@ -0,0 +1,16 @@
+/*
+
+string name = "proceduralWay.js";
+string email = "asispal274@gmail.com";
+string date = "2021/07/18";
+
+user createUser(data){};
+user updateUser(data){};
+user deleteUser(data){};
+
+
+string name2 = "proceduralWay.js";
+string email2 = " asispal@gmail.com";
+string date2 = "2021/07/18";
+
+*/
